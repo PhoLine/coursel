9ba2a985c71ec15c59317747a72da6b7cdc5b033
PhoLine <34483588+PhoLine@users.noreply.github.com>
2018-05-20 06:52:47 +0700

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,1 +1,131 @@
-# coursel
+<?php
+/*
+copyright @ medantechno.com
+2017
+*/
+require_once('./line_class.php');
+$channelAccessToken = 'bWsDmsS8yeXGwlxLHODjcoDtf89txJed+K+h1NtnnNBPAC37f8uistb1GnNwTFq61d7A8na8aB/f2x0SogCrIy6ecvbqQR+BaWPncSyuqgUifdaCdbagI1ZCPgOmn+iCWNwQBPT7FvCDHZW+mKdSrwdB04t89/1O/w1cDnyilFU='; //sesuaikan 
+$channelSecret = 'c599d9653a12ce272e049d775678bf9b';//sesuaikan
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+$userId 	= $client->parseEvents()[0]['source']['userId'];
+$replyToken = $client->parseEvents()[0]['replyToken'];
+$timestamp	= $client->parseEvents()[0]['timestamp'];
+$message 	= $client->parseEvents()[0]['message'];
+$messageid 	= $client->parseEvents()[0]['message']['id'];
+$profil = $client->profil($userId);
+$pesan_datang = $message['text'];
+//pesan bergambar
+if($message['type']=='text')
+{
+	if($pesan_datang=='/Store') {
+		
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+array (
+  'type' => 'template',
+  'altText' => 'this is a carousel template',
+  'template' => 
+  array (
+    'type' => 'carousel',
+    'columns' => 
+    array (
+      0 => 
+      array (
+        'thumbnailImageUrl' => 'https://example.com/bot/images/item1.jpg',
+        'imageBackgroundColor' => '#FFFFFF',
+        'title' => 'Item',
+        'text' => 'Percobaan doang mamank',
+        'defaultAction' => 
+        array (
+          'type' => 'uri',
+          'label' => 'View detail',
+          'uri' => 'http://example.com/page/123',
+        ),
+        'actions' => 
+        array (
+          0 => 
+          array (
+            'type' => 'postback',
+            'label' => 'Buy',
+            'data' => 'action=buy&itemid=111',
+          ),
+          1 => 
+          array (
+            'type' => 'postback',
+            'label' => 'Add to cart',
+            'data' => 'action=add&itemid=111',
+          ),
+          2 => 
+          array (
+            'type' => 'uri',
+            'label' => 'View detail',
+            'uri' => 'http://example.com/page/111',
+          ),
+        ),
+      ),
+      1 => 
+      array (
+        'thumbnailImageUrl' => 'https://example.com/bot/images/item2.jpg',
+        'imageBackgroundColor' => '#000000',
+        'title' => 'this is menu',
+        'text' => 'description',
+        'defaultAction' => 
+        array (
+          'type' => 'uri',
+          'label' => 'View detail',
+          'uri' => 'http://example.com/page/222',
+        ),
+        'actions' => 
+        array (
+          0 => 
+          array (
+            'type' => 'postback',
+            'label' => 'Buy',
+            'data' => 'action=buy&itemid=222',
+          ),
+          1 => 
+          array (
+            'type' => 'postback',
+            'label' => 'Add to cart',
+            'data' => 'action=add&itemid=222',
+          ),
+          2 => 
+          array (
+            'type' => 'uri',
+            'label' => 'View detail',
+            'uri' => 'http://example.com/page/222',
+          ),
+        ),
+      ),
+    ),
+    'imageAspectRatio' => 'rectangle',
+    'imageSize' => 'cover',
+  ),
+)
+							)
+						);
+				
+	}
+	if($pesan_datang=='Hi'){
+		
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => 'Halo'
+									)
+							)
+						);
+				
+	}
+}
+ 
+$result =  json_encode($balas);
+//$result = ob_get_clean();
+file_put_contents('./balasan.json',$result);
+$client->replyMessage($balas);
+?>
